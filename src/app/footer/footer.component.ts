ef40edf0392c22b27cadab2ef9e7a6c682af1f9e
rajasaikiran <rajasaikiranvemula.com>
2020-12-02 22:34:30 +0530

    Job project

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+// import {TestService}from '../test.service'
+
+@Component({
+  selector: 'app-footer',
+  templateUrl:'./footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+
+  // address:object={};
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+    // this.address=this.ts.getAddress();
+  }
+
+  
+
+}
---
+++ src/app/jobs/jobs.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {TestService} from '../test.service'
+import { Router} from '@angular/router'
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.css']
+})
+export class JobsComponent implements OnInit {
+
+// take it as empty
+
+   TestData:any=[];
+
+  constructor( public rt:Router, public ts:TestService) { }
+
+
+  
+
+  ngOnInit(): void {
+  this.ts.getData().subscribe(
+    (data)=>{this.TestData=data
+    // console.log("jobcomponent dat.....................",this.TestData);
+    
+   },
+    (err)=>{console.log(err)}
+  )
+  }
+
+  clickhere(id){
+
+    // console.log("id.......",id);
+    this.rt.navigateByUrl("/viewuser/"+ id)
+    // now navigating through url menstion url params in routhing module =>component path
+
+    
+  }
+
+}
